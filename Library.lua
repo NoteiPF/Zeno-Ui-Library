c992b7e9bae8c2f1f177a9462b55559ddc860654
NoteiPF <116490370+NoteiPF@users.noreply.github.com>
2023-04-18 16:01:12 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3119,8 +3119,8 @@ function Library:CreateWindow(...)
 
             local GroupboxLabel = Library:CreateLabel({
                 Size = UDim2.new(1, 0, 0, 18);
-                Position = UDim2.new(0, 4, 0, -5);
-                TextSize = 16;
+                Position = UDim2.new(0, 4, 0, -8.5);
+                TextSize = 15;
                 Text = Info.Name;
                 TextXAlignment = Enum.TextXAlignment.Left;
                 ZIndex = 5;
