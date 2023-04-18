9601a9d7da2196cd2c6f476071e21a2464fb6b2e
NoteiPF <116490370+NoteiPF@users.noreply.github.com>
2023-04-18 17:44:55 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2940,9 +2940,9 @@ function Library:CreateWindow(...)
 
     local TabArea = Library:Create('Frame', {
         BackgroundTransparency = 1;
-        Position = UDim2.new(0, 0, 0, 0);
+        Position = UDim2.new(0, 0, 0, 8);
         Size = UDim2.new(1, -16, 0, 21);
-        ZIndex = 2;
+        ZIndex = 1;
         Parent = MainSectionInner;
     });
 
@@ -2956,9 +2956,9 @@ function Library:CreateWindow(...)
     local TabContainer = Library:Create('Frame', {
         BackgroundColor3 = Library.MainColor;
         BorderColor3 = Library.OutlineColor;
-        Position = UDim2.new(0, 0, 0, 0);
+        Position = UDim2.new(0, 0, 0, 30);
         Size = UDim2.new(1, -16, 1, -38);
-        ZIndex = 2;
+        ZIndex = 1;
         Parent = MainSectionInner;
     });
 
