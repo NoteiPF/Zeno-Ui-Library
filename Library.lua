419b1da76f363dbe15d699020762592a4ae75f83
NoteiPF <116490370+NoteiPF@users.noreply.github.com>
2023-04-18 15:18:48 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3122,7 +3122,8 @@ function Library:CreateWindow(...)
                 Position = UDim2.new(0, 4, 0, 2);
                 TextSize = 14;
                 Text = Info.Name;
-                TextXAlignment = Enum.TextXAlignment.Left;
+                TextXAlignment = Enum.TextXAlignment.Center;
+                TextYAlignment = Enum.TextYAlignment.Top;
                 ZIndex = 5;
                 Parent = BoxInner;
             });
